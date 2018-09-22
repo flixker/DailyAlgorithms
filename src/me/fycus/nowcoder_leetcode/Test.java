8ae8539d1b7e1eecc8fcec29f4fd8228feed0266
fycus <xxxyyyzhu@gmail.com>
2018-09-22 10:06:27 +0800

    JumpGame

--- src/me/fycus/nowcoder_leetcode/Test.java
+++ src/me/fycus/nowcoder_leetcode/Test.java
@@ -1,26 +1,11 @@
 package me.fycus.nowcoder_leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Test {
-    int a = 0;
-    volatile boolean flag = false;
-
-    public void writer(){
-        System.out.print(flag);
-    }
-
-    public void reader(){
-        flag = true;
-    }
     public static void main(String[] args){
-        System.out.println(Hello.class);
+        Lock lock = new ReentrantLock();
     }
-}
 
-class Hello{
-    static{
-        System.out.println("Hello");
-    }
 }
