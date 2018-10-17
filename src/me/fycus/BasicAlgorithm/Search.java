bd29a04d15abb4bde837a6970dcbf9bbd0589f0f
xflicker <xxxyyyzhu@gmail.com>
2018-10-17 20:00:29 +0800

    wordLadders

--- src/me/fycus/BasicAlgorithm/Search.java
+++ src/me/fycus/BasicAlgorithm/Search.java
@@ -2,12 +2,14 @@ package me.fycus.BasicAlgorithm;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Arrays;
+
 public class Search {
     public static void main(String[] args){
         Integer[] data = new Integer[]{new Integer(1), new Integer(2),
         new Integer(3), new Integer(7), new Integer(25), new Integer(30)};
-        System.out.println(binaryRecursion(data, 26, 0, data.length - 1));
-        System.out.println(binaryRecursion(data, 7, 0, data.length - 1));
+        System.out.println(fibonacci(data, 26));
+        System.out.println(fibonacci(data, 25));
     }
     public static int sequence(Comparable[] data, Comparable target){
         for(int i = 0; i < data.length; i++){
@@ -19,7 +21,7 @@ public class Search {
 
     public static int binary(Comparable[] data, Comparable target){
         int low = 0;
-        int high = data.length;
+        int high = data.length - 1;
         int mid;
         while(low <= high){
             mid = (low + high) / 2;
@@ -51,7 +53,45 @@ public class Search {
         throw new NotImplementedException();
     }
 
-    public static int fibonacci(Comparable[] data, Comparable target){
+    private static int fibonacciRank = 20;
+    public static int[] generateFibonacci(){
+        int[] result = new int[fibonacciRank];
+        result[0] = 0;
+        result[1] = 1;
+        for(int i = 2; i < fibonacciRank; i++)
+            result[i] = result[i - 1] + result[i - 2];
+        return result;
+    }
 
+    public static int fibonacci(Comparable[] data, Comparable target){
+        int[] fibonacci = generateFibonacci();
+        int k = 0;
+        int len = data.length;
+        while(fibonacci[++k] - 1 < len);
+        Comparable[] array = Arrays.copyOf(data, fibonacci[k]);
+        for(int i = len; i < array.length; i++)
+            array[i] = null;
+        int low = 0;
+        int high = array.length;
+        while(low <= high){
+            int mid = low + fibonacci[k - 1] - 1;
+            if(array[mid] == null){
+                high = mid - 1;
+                k -= 1;
+                continue;
+            }
+            int result = array[mid].compareTo(target);
+            if(result < 0) {
+                low = mid + 1;
+                k -= 2;
+            }
+            else if(result > 0) {
+                high = mid - 1;
+                k -= 1;
+            }
+            else
+                return mid;
+        }
+        return -1;
     }
 }
--- src/me/fycus/nowcoder_leetcode/WordLadder.java
+++ src/me/fycus/nowcoder_leetcode/WordLadder.java
@@ -1,9 +1,72 @@
 package me.fycus.nowcoder_leetcode;
 
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Given two words (start and end), and a dictionary, find the length of shortest transformation sequence from start to end, such that:
+ * Only one letter can be changed at a time
+ * Each intermediate word must exist in the dictionary
+ * <p>
+ * For example,
+ * Given:
+ * start ="hit"
+ * end ="cog"
+ * dict =["hot","dot","dog","lot","log"]
+ * <p>
+ * As one shortest transformation is"hit" -> "hot" -> "dot" -> "dog" -> "cog",
+ * return its length 5.
+ * <p>
+ * Note:
+ * Return 0 if there is no such transformation sequence.
+ * All words have the same length.
+ * All words contain only lowercase alphabetic characters.
+ */
 
 public class WordLadder {
-    public static int ladderLengrh(String start, String end, HashSet<String> dict){
-        return 1;
+    public static void main(String[] args){
+        HashSet<String> dict = new HashSet<>();
+        dict.add("hot");
+        dict.add("dot");
+        dict.add("dog");
+        dict.add("lot");
+        dict.add("log");
+        System.out.print(ladderLength("hit", "cog", dict));
+    }
+    public static int ladderLength(String start, String end, HashSet<String> dict) {
+        int res = 1;
+        Queue<String> queue = new LinkedList<>();
+        queue.add(start);
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            for (int i = 0; i < len; i++) {
+                String cur = queue.poll();
+                if (isDiffOne(cur, end))
+                    return ++res;
+                Iterator<String> it = dict.iterator();
+                while(it.hasNext()){
+                    String s = it.next();
+                    if(isDiffOne(s, cur)){
+                        queue.add(s);
+                        it.remove();
+                    }
+                }
+            }
+            res++;
+        }
+        return 0;
+    }
+
+    // default: s1.length() == s2.length()
+    public static boolean isDiffOne(String s1, String s2) {
+        int len = s1.length();
+        int count = 0;
+        for (int i = 0; i < len; i++) {
+            if (s1.charAt(i) != s2.charAt(i))
+                count++;
+        }
+        return count == 1;
     }
 }
